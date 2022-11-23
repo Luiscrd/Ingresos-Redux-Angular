8715b68aa23e27342cc3e79046aac220ece2cdb8
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-23 20:49:55 +0100

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresComponent } from './ingres/ingres.component';
+import { StadisticComponent } from './ingres/stadistic/stadistic.component';
+import { DetailComponent } from './ingres/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NabvarComponent } from './shared/nabvar/nabvar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresComponent,
+    StadisticComponent,
+    DetailComponent,
+    FooterComponent,
+    NabvarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingres/ingres.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IngresComponent } from './ingres.component';
+
+describe('IngresComponent', () => {
+  let component: IngresComponent;
+  let fixture: ComponentFixture<IngresComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IngresComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IngresComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
